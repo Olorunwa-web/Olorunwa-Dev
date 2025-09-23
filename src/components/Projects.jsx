39ae38bf3e7021c642ac52f84308d5b02e65f7fb
olorunwa-web <olorunwamoliki@gmail.com>
2025-09-23 22:17:25 +0100

    Initial commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import { projects } from '../lib/db'
 // import popup from '../assets/pop-out-svgrepo-com.svg'
-import Icon from '../components/Icon'
+import Icon from '../components/Icon';
+
+
+
 const Projects = () => {
 
   
-
   return (
     <>
       <section className='mt-7 '>
-        <section className='mx-auto w-full max-w-screen-2xl md:px-20 px-4'>
+        <section className='mx-auto w-full max-w-screen-3xl md:px-18 px-4'>
             <h1 className='text-white font-semibold text-3xl font-jakarta'>Work</h1>
             <p className='w-full tracking-wide italic mt-2 text-white font-normal leading-6 text-sm font-jakarta'>Check out some of my recent work-cool products I've been part of, built using the latest tech and libraries to create something awesome!</p>
-            <section className='mt-10 w-full flex flex-col justify-between flex-wrap md:flex-row gap-y-15'>
+            <section className='mt-12 lg:mt-16 w-full flex flex-col justify-between flex-wrap md:flex-row gap-y-15'>
               {projects.map((project)=> {
                 return (
                   <div key={project.id} className={`flex flex-col gap-10 ${project.id % 2 === 0 ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
-                    <div className={` ${project.id % 2 === 0 ? "w-full lg:basis-2/3": "w-full lg:basis-2/3"}  `}>
-                      {/* <img src= {project.image} alt="" className='w-full rounde bg-white' /> */}
-                      <video className='w-full h-full' src= {project.image} autoPlay loop muted playsInline ></video>
+                    <div className={` ${project.id % 2 === 0 ? "w-full lg:basis-2/3": "w-full lg:basis-2/3 "}  `}>
+                      <video className='w-full h-ful' src= {project.image} autoPlay preload = 'none' loop muted playsInline ></video>
                     </div>
-                    <div className={` ${project.id % 2 === 0 ? "w-full lg:basis-1/2": "w-full lg:basis-1/2 p-3"} flex flex-col gap-3`}>
-                      <h1 className='text-white font-semibold text-lg mt- lg:mt-0 font-jakarta'>{project.name}</h1>
+                    <div className={` ${project.id % 2 === 0 ? "w-full lg:basis-1/2 lg:px-3": "w-full lg:basis-1/2 lg:px-3 "} flex flex-col gap-3`}>
+                      <h1 className='text-white font-bold text-lg mt- lg:mt-0 font-jakarta'>{project.name}</h1>
                       <p className='w-full tracking-wide italic mt- text-[#909090] font-normal leading-6 text-sm font-jakarta'>{project.description}</p>
                       <div className='flex flex-wrap gap-3 mt-3'>
                         {project.technologies.map((tech, index) => (
@@ -31,12 +32,20 @@ const Projects = () => {
                           </div>
                         ))}
                       </div>
-                      <div>
-                        <Icon/>
+                      <h1 className='mt-4 text-white text-base font-normal font-jakarta'>Features:</h1>
+                      <div className='flex flex-col gap-3'>
+                         {project.features?.map((statement, idx) =>(
+                          <div key={idx} className='flex  gap-2'>
+                            <div className='pt-1'>
+                              <Icon/>
+                            </div>
+                            <span className = 'w-full tracking-wide italic  text-[#909090] font-normal leading-6 text-sm font-jakarta'>{statement}</span>
+                          </div>
+                         ))}
                       </div>
-                      <div className='mt-4'>
+                      {/* <div className='mt-4'>
                         <a href={project.liveLink} target='_blank' rel='noreferrer' className='text-sm text-[#1BF273] font-jakarta underline'>View Project</a>
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 )
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 
-import Furniturelanding from '../assets/Screenshot (938).png';
+// import Furniturelanding from '../assets/Screenshot (938).png';
 
 // =============================Icons============================
 
@@ -20,43 +20,48 @@ export const projects = [
       id: 1,
       name: "HR MANAGER",
       image: "https://res.cloudinary.com/dyz6uugbr/video/upload/v1758640114/Sign_In_-_Google_Chrome_2025-09-21_06-21-41_psqp2v.mp4",
-      description: "A modern HR management platform designed to streamline employee onboarding, payroll, performance tracking, and leave management. It provides managers with powerful tools to monitor workforce productivity, handle approvals, and ensure compliance, while offering employees a seamless self-service experience.",
+      description: "A modern HR management app streamlining recruitment, employee tracking, and performance monitoring. It provides an intuitive dashboard for HR personnel to manage records, approvals, and team workflows",
       technologies: [ {title: "ReactJS", icon: reactIcon  }, {title: "NodeJS", icon: NodejsIcon  }, {title: "MongoDB", icon: mongoDBIcon }, {title: "Express", icon: expressIcon  }, {title: "Tailwind CSS", icon: tailwindIcon  }, {title: "Framer Motion", icon: framerIcon }],
       liveLink: "https://mern-app-dp5x.vercel.app/",
+      features: [ 'Admin Panel: Manage employees, departments, roles, leave requests, and notifications.','Recruitment & Onboarding: Streamlined hiring workflow with candidate tracking, interview scheduling, and onboarding checklists.', 'Performance & Feedback: Monitor KPIs, collect feedback, and generate reports for employee performance evaluation.'],
     
     },
     {
       id: 2,
       name: "VENTURESPHERE LANDING PAGE",
       image: "https://res.cloudinary.com/dyz6uugbr/video/upload/v1758642749/Ventsphere_-_Google_Chrome_2025-05-13_15-20-22_syjxgr.mp4",
-      description: "A sleek, responsive landing page designed with modern UI principles, delivering seamless user interaction and an engaging frontend experience.Venturesphere’s landing page showcases a modern platform for innovation and collaboration. It highlights key features, success stories, and opportunities for startups and investors. Testimonials and success stories build credibility and trust.",
+      description: "A sleek, responsive landing page designed with modern UI principles, delivering seamless user interaction and an engaging frontend experience.Venturesphere’s landing page showcases a modern platform for innovation and collaboration.",
       technologies: [ {title: "ReactJS", icon: reactIcon  }, {title: "TypeScript", icon: typescriptIcon  }, {title: "Tailiwnd CSS", icon: tailwindIcon  }, {title: "Framer Motion", icon: framerIcon  }],
       liveLink: "https://venturesphere-app.vercel.app/",
+      features: [ 'Responsive Design: Fully adaptive layout for desktops, tablets, and mobile devices, ensuring a seamless user experience across screen sizes.','Interactive Animations: Smooth hover effects, scroll-triggered animations, and engaging transitions to enhance visual appeal and user engagement.', 'Dynamic Content Rendering: Modular components for showcasing projects, team members, and testimonials with easy updates and efficient reusability'],
     },
     {
       id: 3, 
       name: "FMCG (PZ Cussons Revamp)",
       image: "https://res.cloudinary.com/dyz6uugbr/video/upload/v1758640455/FMGC_-_Google_Chrome_2025-09-21_06-28-50_emckvt.mp4",
-      description: "The revamped FMCG App (PZ Cussons) is a modernized digital solution designed to streamline products, brands, and customer engagement. It offers an intuitive interface for showcasing products and brands in real time. The app also enhances visibility into performance with insightful dashboards and reports. With a user-friendly design, it ensures smoother navigation and accessibility across devices. Overall, it strengthens efficiency and customer satisfaction within the FMCG ecosystem ",
+      description: "The revamped (PZ Cussons) FMCG is a modernized digital solution designed to showcase products, brands, and customer engagement in real time. The app also enhances visibility into performance with insightful dashboards and reports.",
       technologies: [ {title: "ReactJS", icon: reactIcon  }, {title: "NodeJS", icon: NodejsIcon  }, {title: "MongoDB", icon: mongoDBIcon }, {title: "Express", icon: expressIcon  }, {title: "Tailwind CSS", icon: tailwindIcon  }, {title: "Framer Motion", icon: framerIcon }],
       liveLink: "https://fmgc-client-edat.vercel.app/",
+      features: [ 'Interactive Product Catalog: Sleek, responsive UI for browsing products with category filters, real-time search, and detailed product views.', 'Filtering: Filter products by category, price, brand, or availability, for quick access to exactly what you need.'],
     },
     {
       id: 4,
       name: "AGRINEX",
       image: "https://res.cloudinary.com/dyz6uugbr/video/upload/v1758639323/AgriNex_-_Google_Chrome_2025-09-21_06-31-53_ccgpoi.mp4",
-      description: "Agrinex is a forward-thinking agribusiness solutions provider dedicated to advancing sustainable farming through innovation, technology, and strategic partnerships. The app simplifies record-keeping and tracks farming activities seamlessly. It connects farmers with markets, suppliers, and expert advisory services. Agrinex helps modernize agriculture by making farming smarter, efficient, and sustainable.",
+      description: "Agrinex is a forward-thinking agribusiness solutions provider dedicated to advancing sustainable farming through innovation, technology, and strategic partnerships. It connects farmers with markets, suppliers, and expert advisory services.",
       technologies: [ {title: "ReactJS", icon: reactIcon  }, {title: "Tailwind CSS", icon: tailwindIcon  }, {title: "EmailJS", icon: emailjsIcon  }],
       liveLink: "https://agri-nex-app.vercel.app/",
-    },
-    {
-      id: 5,
-      name: "FURNITURE LANDING PAGE",
-      image: "https://res.cloudinary.com/dyz6uugbr/video/upload/v1758642749/Ventsphere_-_Google_Chrome_2025-05-13_15-20-22_syjxgr.mp4",
-      description: "A sleek and modern landing page for a furniture store, showcasing products and promotions.",
-      technologies: [ {title: "ReactJS", icon: reactIcon  }, {title: "Tailwind CSS", icon: tailwindIcon  }],
-      liveLink: "https://furniture-landing-page-henna.vercel.app/",
+      features: [ 'Built with React and Tailwind CSS for a fast, modern, and fully responsive user interface across all devices.', "Forms: Dynamic contact and feedback forms with real-time validation", 'Email Automation: EmailJS integration for instant email notifications, auto-responses, and seamless communication with users'],
 
     },
+    // {
+    //   id: 5,
+    //   name: "FURNITURE LANDING PAGE",
+    //   image: "https://res.cloudinary.com/dyz6uugbr/video/upload/v1758642749/Ventsphere_-_Google_Chrome_2025-05-13_15-20-22_syjxgr.mp4",
+    //   description: "A sleek and modern landing page for a furniture store, showcasing products and promotions.",
+    //   technologies: [ {title: "ReactJS", icon: reactIcon  }, {title: "Tailwind CSS", icon: tailwindIcon  }],
+    //   liveLink: "https://furniture-landing-page-henna.vercel.app/",
+
+    // },
   ];
 

44e35b3a15da412235c0c69be58cdc5f6b81a9d6
olorunwa-web <olorunwamoliki@gmail.com>
2025-09-24 05:43:10 +0100

    preanno commit

---
+++ src/components/Icon2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Icon2 = () => {
+  return (
+    <>
+      <div className='hover:text-[#1BF273] text-white transition-all duration-300'>
+        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+         <path d = "M 432 320 H 400 a 16 16 0 0 0 -16 16 V 448 H 64 V 128 H 208 a 16 16 0 0 0 16 -16 V 80 a 16 16 0 0 0 -16 -16 H 48 A 48 48 0 0 0 0 112 V 464 a 48 48 0 0 0 48 48 H 400 a 48 48 0 0 0 48 -48 V 336 A 16 16 0 0 0 432 320 Z M 488 0 h -128 c -21.37 0 -32.05 25.91 -17 41 l 35.73 35.73 L 135 320.37 a 24 24 0 0 0 0 34 L 157.67 377 a 24 24 0 0 0 34 0 L 435.28 133.32 L 471 169 c 15 15 41 4.5 41 -17 V 24 A 24 24 0 0 0 488 0 Z">
+         </path>
+        </svg>
+      </div>
+    </>
+  )
+}
+
+export default Icon2
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { projects } from '../lib/db'
 // import popup from '../assets/pop-out-svgrepo-com.svg'
 import Icon from '../components/Icon';
-
+import { motion as Motion } from 'framer-motion';
+import Icon2 from '../components/Icon2';
 
 
 const Projects = () => {
@@ -14,16 +15,26 @@ const Projects = () => {
         <section className='mx-auto w-full max-w-screen-3xl md:px-18 px-4'>
             <h1 className='text-white font-semibold text-3xl font-jakarta'>Work</h1>
             <p className='w-full tracking-wide italic mt-2 text-white font-normal leading-6 text-sm font-jakarta'>Check out some of my recent work-cool products I've been part of, built using the latest tech and libraries to create something awesome!</p>
-            <section className='mt-12 lg:mt-16 w-full flex flex-col justify-between flex-wrap md:flex-row gap-y-15'>
+            <section className='mt-12 lg:mt-16 w-full flex flex-col justify-between flex-wrap md:flex-row gap-y-15 lg:gap-y-19'>
               {projects.map((project)=> {
+                 const isEven = project.id % 2 === 0;
+
                 return (
                   <div key={project.id} className={`flex flex-col gap-10 ${project.id % 2 === 0 ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
-                    <div className={` ${project.id % 2 === 0 ? "w-full h-full lg:basis-2/3": "w-full   lg:basis-2/3 "}  `}>
-                      <video className='w-full h-full'  autoPlay  loop muted playsInline  preload='none' > <source src={project?.image}  type='video/mp4' /> </video>
-                      {/* <video className='w-full h-full' src= {project.image} autoPlay loop muted playsInline ></video> */}
-                    </div>
-                    <div className={` ${project.id % 2 === 0 ? "w-full lg:basis-1/2 lg:px-3": "w-full lg:basis-1/2 lg:px-3 "} flex flex-col gap-3`}>
-                      <h1 className='text-white font-bold text-lg mt- lg:mt-0 font-jakarta'>{project.name}</h1>
+                    <Motion.div 
+                    //  initial={{ opacity: 0, x: isEven ? 40 : -40 }}
+                    //  whileInView={{ opacity: 1, x: 0 }}
+                    //  transition={{ duration: 1, ease: [0, 0.71, 0.2, 1.01] }}
+                    //  viewport={{ once: true, amount: 0.3 }}
+
+                    className={`relative group overflow-x-hidden ${isEven ? "w-full h-full lg:basis-2/3": "w-full   lg:basis-2/3 "}  `}>
+                      <video className='w-full h-full '  autoPlay  loop muted playsInline  preload='none'> <source src={project?.image}  type='video/mp4' /> </video>
+                      <a href={project.liveLink} target='_blank' rel='noreferrer' className={`absolute top-3 ${isEven ? "-left-9 hidden lg:block" : "-right-9 hidden lg:block"} `}>
+                          <Icon2/>
+                        </a>
+                    </Motion.div>
+                    <div className={` ${isEven === 0 ? "w-full lg:basis-1/2 lg:px-3": "w-full lg:basis-1/2 lg:px-3 "} flex flex-col gap-3`}>
+                      <h1 className='text-white font-bold text-lg mt-0 lg:mt-3 font-jakarta'>{project.name}</h1>
                       <p className='w-full tracking-wide italic mt- text-[#909090] font-normal leading-6 text-sm font-jakarta'>{project.description}</p>
                       <div className='flex flex-wrap gap-3 mt-3'>
                         {project.technologies.map((tech, index) => (
@@ -44,9 +55,9 @@ const Projects = () => {
                           </div>
                          ))}
                       </div>
-                      {/* <div className='mt-4'>
-                        <a href={project.liveLink} target='_blank' rel='noreferrer' className='text-sm text-[#1BF273] font-jakarta underline'>View Project</a>
-                      </div> */}
+                      <div>
+                        
+                      </div>
                     </div>
                   </div>
                 )

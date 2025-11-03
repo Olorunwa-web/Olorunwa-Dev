926052e73a72d84b68d5c109b5a68e4cc3ae591c
olorunwa-web <olorunwamoliki@gmail.com>
2025-11-04 00:56:15 +0100

    first commit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -17,6 +17,7 @@ const AboutMe = () => {
                 duration: 1.4,
                 ease: [0, 0.71, 0.2, 1.01],
             }}
+            viewport={{ amount: 0.3}}
             className = 'mx-auto w-full max-w-screen-xl md:px-20 px-4'>
             <h1 className='text-white font-semibold text-center text-3xl font-jakarta'>Behind the <span className='bg-[linear-gradient(20deg,#2E86D0_0%,#1BF273_35%,#2CE1C6_68%,#2E86D0_100%)] bg-clip-text text-transparent'>Code</span></h1>
             <p className='w-full tracking-wide italic mx-auto md:w-[90%] mt-7 text-white font-normal leading-6 text-sm font-jakarta'>I'm a Front-end Developer based in Lagos, Nigeria, with almost 2 years of experience. My journey started at Tech Studio Academy, where I gained a strong foundation in modern web technologies and had chance to build some cool projects that taught me a ton. I began with HTML, CSS, Javascript and then swithed to React where I had the opportunity to work on projects from scratch that really shaped my skills in JS and UI development.</p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,30 +11,32 @@ const Home = () => {
     const details = {name: "I'm Olorunwa", description: "I craft seamless and responsive" , description2: "that transform ideas into engaging digital experiences"}
 
     const defaultAnimation = {
-      hidden: { opacity: 0, y: 90 },
-      visible: { opacity: 1, y: 0, transition: { duration: 1.4  }},
+      hidden: { opacity: 0, x: -40 },
+      visible: { opacity: 1, x: 0, transition: { duration: 0.5, delay: 0.3  }},
     }
 
   return (
     <>
       <Motion.section 
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3, delay: 1 }}
+        // initial={{ opacity: 0 }}
+        // animate={{ opacity: 1 }}
+        // transition={{ duration: 2, delay: 0.4 }}
       className='relative mx-auto mt-11 lg:mt-17 w-full max-w-screen-xl md:px-20 px-4'>
         <section className='w-full flex flex-col lg:flex-row justify-between items-center gap-10'>
-            <Motion.div   className='basis-2/3 w-full flex flex-col gap-9'>
+            <Motion.div 
+             variants={defaultAnimation} initial = "hidden" animate = "visible"
+            className='basis-2/3 w-full flex flex-col gap-9'>
                 <div className='flex flex-col gap-4'>
-                    <Motion.span  variants={defaultAnimation} initial = "hidden" animate = "visible" className='text-white font-jakarta flex items-center font-light text-2xl md:text-3xl'>Hi<img src={wavingIcon} alt="" className='w-7 md:w-9' />, {details.name} </Motion.span>
+                    <Motion.span   className='text-white font-jakarta flex items-center font-light text-2xl md:text-3xl'>Hi<img src={wavingIcon} alt="" className='w-7 md:w-9' />, {details.name} </Motion.span>
                     <div >
                        <span className='text-white font-jakarta font-bold leading-9 md:leading-11 text-3xl md:text-4xl'>{details.description} <span className='bg-[linear-gradient(20deg,#2E86D0_0%,#1BF273_35%,#2CE1C6_68%,#2E86D0_100%)] bg-clip-text text-transparent'> user interfaces </span>{details.description2}</span>
                     </div>
                 </div>
                 <div className='flex flex-wrap gap-5'>
-                  <Motion.button drag whileDrag={{ zIndex: 99 }} className=' border-1 py-2 px-4 flex items-center gap-3 border-[#878787] px-3 rounded-full '>
+                  <button className=' border-1 py-2 px-4 flex items-center gap-3 border-[#878787] px-3 rounded-full '>
                     <span className='w-[8px] h-[8px] animate-ping bg-[#1BF291] rounded-full'></span>
                     <span className='text-white text-sm font-jakarta '>{work.avail} <span className='text-[#878787]'>{work.ready}</span></span>
-                 </Motion.button>
+                 </button>
                  <div>
                   <a href="mailto:olorunwamoliki@gmail.com" className='flex items-center gap-2 bg-[#2CE1C6] p-2 rounded-full text-black text-base font-medium font-jakarta'>Contact me <div className='w-2 h-2 bg-black mt-[2px] rounded-full'></div></a>
                  </div>
@@ -42,7 +44,14 @@ const Home = () => {
             </Motion.div>
             <div className='basis-1/2 w-full'>
                <div className='w-full flex lg:justify-end'>
-                  <img src= {roundedImage} alt="" className='w-full' />
+                  <Motion.img
+                   initial = {{ opacity: 0, y: 70}}
+                 animate = {{opacity: 1, y: 0}}
+                 transition={{ 
+                  duration: 0.8,
+                  delay: 0.3,                  
+                 }}
+                  src= {roundedImage} alt="" className='w-full' />
                </div>
             </div>
         </section>
